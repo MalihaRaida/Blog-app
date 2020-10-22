dd50dbe5354ed7b3e6e8bcc921bab6722be93138
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2020-10-22 11:27:31 +0600

    Signup screen design without authenticaton

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { ImageBackground,StyleSheet, Text, View } from 'react-native';
 import { Card,Input, Button } from 'react-native-elements';
-
+import { AntDesign,MaterialIcons  } from '@expo/vector-icons';
 const LoginScreen =(props)=> {
     return(
         <View style={styles.container}>
@@ -14,23 +14,30 @@ const LoginScreen =(props)=> {
             style={styles.image}
             source={require('./../../assets/coast-rocks-blue-green-sea-nature-small.jpg')}>
             <Text style={styles.text}>TRAVELS</Text>
-            <View
-            style={styles.logo}/>
-            <Card containerStyle={styles.card}>
-                <Card.Title>Log In</Card.Title>
+        
+            <Card containerStyle={styles.card} >
+                <Card.Title style={{fontSize:20}}>Log In</Card.Title>
                 <Card.Divider />
                 <Input
-                leftIcon={{type: 'font-awesome', style:{marginRight:5},name: 'envelope'}}
+                leftIcon={<MaterialIcons name="mail-outline" size={24} color="black"/>}
                 placeholder="E-mail Address"/>
                 <Input 
-                leftIcon={{type:'font-awesome' ,style:{marginRight:5}, name: 'key'}}
-                placeholder="Password" secureTextEntry={true} />
+                leftIcon={<MaterialIcons name="vpn-key" size={24} color="black"/>}
+                placeholder="Password" 
+                secureTextEntry={true} />
                 <Button
-                title="  Sign In!"
-                type="solid"/>
+                icon={<AntDesign name="login" size={24} color="white" />}
+                titleStyle={{paddingLeft:10}}
+                title="Log In!"
+                type="solid"
+                />
                 <Button
                 type="clear"
-                title="  Don't have an account?"/>
+                title="Join as a new member"
+                onPress={function () {
+                    props.navigation.navigate("Sign Up");
+                }}
+                />
             </Card>
             </ImageBackground>
             
@@ -40,14 +47,7 @@ const LoginScreen =(props)=> {
 }
 
 const styles=StyleSheet.create({
-    logo:{
-        borderBottomColor: '#FFFFFF',
-        borderBottomWidth: 5,
-        marginLeft:50,
-        marginRight:50,
-        marginBottom:70
-    },
-    container:{
+        container:{
         flex:1,
         justifyContent: "center",
         
@@ -64,7 +64,8 @@ const styles=StyleSheet.create({
         paddingBottom:10,
     },
     card:{
-        
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        fontFamily:"san-serif",
         borderRadius:10
     }
     
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,29 +1,79 @@
 import React,{useState} from 'react';
-import { StyleSheet, Text, View ,Button} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { ImageBackground,StyleSheet, Text, View } from 'react-native';
+import { Card,Input, Button } from 'react-native-elements';
+import { AntDesign,Ionicons,MaterialIcons,Feather  } from '@expo/vector-icons';
 
 const SignupScreen =(props)=> {
     return(
-        <View>
-            <Text style={styles.TextStyle}>
-            Hello from HomeScreen
-            </Text>
-            <Button 
-            title="Log In"
-            onPress={
-                function(){
-                props.navigation.navigate('Log In');
-                }}>
-            </Button>
+        <View style={styles.container}>
+            <StatusBar
+            hidden={true}
+            backgroundColor="blue"
+            barStyle="light-content"/>
+            <ImageBackground
+            style={styles.image}
+            source={require('./../../assets/coast-rocks-blue-green-sea-nature-small.jpg')}>
+            <Text style={styles.text}>TRAVELS</Text>
+            <Card containerStyle={styles.card}>
+                <Card.Title style={{fontSize:20}}>Sign Up</Card.Title>
+                <Card.Divider />
+                <Input 
+                leftIcon={<Ionicons name="ios-person" size={24} color="black" />}
+                placeholder="Username" />
+                <Input 
+                leftIcon={<Ionicons name="md-school" size={24} color="black" />}
+                placeholder="StudentID" />
+                <Input
+                leftIcon={<MaterialIcons name="mail-outline" size={24} color="black"/>}
+                placeholder="E-mail Address"/>
+                <Input 
+                leftIcon={<MaterialIcons name="vpn-key" size={24} color="black"/>}
+                placeholder="Password" 
+                secureTextEntry={true} />
+                
+                <Button
+                icon={<Feather name="user" size={25} color="white" />}
+                titleStyle={{paddingLeft:10}}
+                title="Sign up!"
+                type="solid"
+                />
+                <Button
+                type="clear"
+                title="Already have an account"
+                onPress={function () {
+                    props.navigation.navigate("Log In");
+                }}
+                />
+            </Card>
+            </ImageBackground>  
         </View>
-    
         );
 }
 
 const styles=StyleSheet.create({
-    TextStyle:{
-        fontSize:30,
-        color:'blue',
+
+    container:{
+        flex:1,
+        justifyContent: "center",  
     },
+    image:{
+        flex:1,
+        justifyContent: "center",
+    },
+    text:{
+        textAlignVertical: "center",
+        textAlign: "center",
+        color:"#FFFFFF",
+        fontSize:30,
+        paddingBottom:10,
+    },
+    card:{
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        fontFamily:"san-serif",
+        borderRadius:10
+    }
+    
 });
 
 export default SignupScreen;
